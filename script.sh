7f5fe7a40e8e999d70b4086630c2ba4d2a857a8e
0noya <luc.gonzales@proton.me>
2024-08-29 14:14:45 +0200

    après création du script

--- script.sh
+++ script.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+echo Où voulez-vous stocker le contenu? Veuillez mettre le chemin absolu
+read
+cd $variable
+mkdir test
+touch collection.txt
+echo Quel film voulez-vous ajouter?
+read
+echo -e '$variable | tee collection.txt
